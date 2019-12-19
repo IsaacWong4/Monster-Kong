35067809e3e285afe5c9e6d08d1938060706d2ea
IsaacWong4 <kidosol3839@gmail.com>
2019-12-19 15:35:23 +0800

    first commit

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var GameState = {
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.scalepageAlignHorizontally = true;
         this.scalepageAlignVertically = true;
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.arcade.gravity.y = 1000; 
     },
 
     //load the game assets before the game starts
@@ -17,5 +20,34 @@ var GameState = {
         this.load.image('arrowButton', 'assets/images/arrowButton.png');
         this.load.image('actionButton', 'assets/images/actionButton.png');
         this.load.image('barrel', 'assets/images/barrel.png');
+
+        this.load.spritesheet('player', 'assets/images/player_spritesheet.png', 28, 30, 5, 1, 1);
+        this.load.spritesheet('fire, 'assets/images/fire spritesheet.png', 20, 21, 2, 1, 1);
+    },
+    //executed after everything is loaded
+    create: function() {
+
+        this.ground = this.add.sprite(0, 500, 'ground');
+        this.game.physics.arcade.enable(this.ground);
+        this.ground.body.allowGravity = false;
+        this.ground.body
+
+        var platform = this.add.sprite(0, 300, 'platform');
+        this.game.physics.arcade.enable(platform);
+        platform.body.allowGravity = false;
+
+        //create player
+        this.player = this.add.sprite(100, 200, 'player', 3);
+        this.player.anchor.setTo(0.5);
+        this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
+        this.game.physics.arcade.enable(this.player);
+
+
+    },
+    update: function() {
+
     }
-}
+
+};
+
+//initiate the Phaser framework
